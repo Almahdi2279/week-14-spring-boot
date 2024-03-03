ec3a9bb5cd5a602655c68ad5eae16644edfb8d71
Almahdi2279 <ibnumahdi@gmail.com>
2024-03-02 23:45:43 -0600

    first commit

---
+++ src/main/java/pet/store/entity/Customer.java
@@ -0,0 +1,39 @@
+package pet.store.entity;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+@Entity
+@Data
+public class Customer {
+	
+	//Identifying the primary key for the customer table
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long customerId;
+	
+	private String customerFirstName;
+	
+	private String customerLastName;
+	
+	@Column(unique = true)
+	private String customerEmail;
+	
+	// Many to many relationship
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+	@ManyToMany(mappedBy = "customers", cascade = CascadeType.ALL)
+	private Set<PetStore> petStores = new HashSet<>();
+
+}
